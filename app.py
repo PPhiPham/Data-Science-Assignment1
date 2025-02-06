6e401f4ec02e3cfe1ca02e99bdb062651bf1bc87
“s3609987” <s3609987@vuw.leiduniv.nl>
2025-02-06 12:20:14 +0100

    init bokeh

--- app.py
+++ app.py
@@ -7,5 +7,12 @@ from bokeh.io import output_file, output_notebook
 from bokeh.plotting import figure, show
 from bokeh.models import ColumnDataSource
 from bokeh.layouts import row, column, gridplot
-from bokeh.models.widgets import Tabs, Panel
+from bokeh.models import Tabs, Panel
 
+output_file('index.html')
+
+# Set up the figure(s)
+fig = figure()  # Instantiate a figure() object
+
+# Preview and save 
+show(fig)  # See what I made, and save if I like it
