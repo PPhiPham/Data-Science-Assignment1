6b1e542a9fe02981a306094b37bad7d06254ae91
“s3609987” <s3609987@vuw.leiduniv.nl>
2025-02-06 12:04:48 +0100

    init commit

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-import bokeh
 import pandas as pd
 import numpy as np
 import geopandas
+
+# Bokeh libraries
+from bokeh.io import output_file, output_notebook
+from bokeh.plotting import figure, show
+from bokeh.models import ColumnDataSource
+from bokeh.layouts import row, column, gridplot
+from bokeh.models.widgets import Tabs, Panel
+
